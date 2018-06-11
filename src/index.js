958fe1b2bf6b8dedaa53d410228c69e3e00e8b1c
Ignacio Ureta <ignacio.ureta@live.com>
2018-06-11 19:28:59 -0400

    added store provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import store from "./redux/store";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const ProviderApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
